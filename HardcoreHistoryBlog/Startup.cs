edd86625972765faff9d042f2a465a73c76a15d2
zaki <zihali91@gmail.com>
2018-12-16 07:01:57 +0000

    Hidden connection string

--- HardcoreHistoryBlog/Startup.cs
+++ HardcoreHistoryBlog/Startup.cs
@@ -18,10 +18,23 @@ namespace HardcoreHistoryBlog
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+     .SetBasePath(env.ContentRootPath)
+     .AddJsonFile("appsettings.json",
+                  optional: false,
+                  reloadOnChange: true)
+     .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
+            Configuration = builder.Build();
         }
+
 
         public IConfiguration Configuration { get; }
 
