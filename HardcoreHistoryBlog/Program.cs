341ee1bb7b303e83533f9f6e1007beef2c6c12e7
zaki <zihali91@gmail.com>
2020-01-15 00:47:01 +0000

    ready to publish

--- HardcoreHistoryBlog/Program.cs
+++ HardcoreHistoryBlog/Program.cs
@@ -5,7 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 
@@ -19,21 +23,27 @@ namespace HardcoreHistoryBlog
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
             .ConfigureAppConfiguration((context, config) =>
             {
-                var root = config.Build();
-                var vault = root["KeyVault:Vault"];
-                
-                if(!string.IsNullOrEmpty(vault))
+                if (context.HostingEnvironment.IsProduction())
                 {
+                    var root = config.Build();
+                    var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                    var keyVaultClient = new KeyVaultClient(
+                        new KeyVaultClient.AuthenticationCallback(
+                            azureServiceTokenProvider.KeyVaultTokenCallback));
+
                     config.AddAzureKeyVault(
-                    $"https://{root["KeyVault:Vault"]}.vault.azure.net/",
-                    root["KeyVault: ClientId"],
-                    root["KeyVault: ClientSecret"]);
+                        $"https://{root["KeyVault:Vault"]}.vault.azure.net/",
+                        keyVaultClient,
+                        new DefaultKeyVaultSecretManager());
                 }
             })
-                .UseStartup<Startup>();
+             .ConfigureWebHostDefaults(webBuilder =>
+             {
+                 webBuilder.UseStartup<Startup>();
+             });
     }
 }
